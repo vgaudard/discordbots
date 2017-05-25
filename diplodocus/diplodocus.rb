ff8aab1ca0d90bf3d17ac7d81632dc2908264869
Victor GAUDARD <victor.gaudard@gmail.com>
2017-05-25 03:10:01 +0200

    Restructured code

--- diplodocus/diplodocus.rb
+++ diplodocus/diplodocus.rb
@@ -1,118 +1,63 @@
 #!/usr/bin/ruby
 
-require 'discordrb'
+require '../root'
 
-tokenFile = File.open("token", "r")
-applicationIdFile = File.open("application_id", "r")
-bot = Discordrb::Bot.new token: tokenFile.read.strip, client_id: applicationIdFile.read.to_i
-tokenFile.close
-applicationIdFile.close
-
-awesomeregex = /(?:\b)(di|dy|cri)(\S{3,}?)\b/i
+awesomeregex = /\b((?:d|cr)[iy])(\S{3,}?)\b/i
 awesomeregexofnodoublon = /^([a-z])\1*(\1.*)$/
 merciregex = /(good|merci|nice|bien|cool|thx|thanks|thank|perfect|parfait|perf).*dip/i
-tagueuleregex = /(ta gueule|tais toi|shut).*dip/i
 notregex = /not|pas/i
-lastmessagetime = -1
+tagueuleregex = /(ta gueule|tais toi|shut).*dip/i
 
-thanks_answers = [  "Oh you! https://giphy.com/gifs/whatever-oh-you-uiVH8ETzr9nZm",
-                    "De rien !",
-                    "ENFIN !!! Ca fait des mois que je reste eveillé à longueur de journée pour votre plaisir, et pas de merci ! Et enfin quelqu'un prend en compte mon existence ! Merci !",
-                    "Ouais, ça mérite au moins ça...",
-                    "Je ne fais que mon travail... http://i3.kym-cdn.com/entries/icons/original/000/014/711/1380697092809.jpg",
-                    "De rien, ma belle !",
-                    "Tu veux qu'on se retrouve après le travail ? http://www.excusememe.com/pics/imagebase/5099.gif",
-                    "Tu veux qu'on se retrouve après le travail ? https://s-media-cache-ak0.pinimg.com/originals/64/24/2f/64242f27f1afa3b669982c641e31bf62.jpg",
-                    "Tu veux qu'on se retrouve après le travail ? http://images.halloweencostumes.com/products/32430/1-2/adult-al-gator-costume.jpg",
-                    "Tu te fous de ma gueule, c'est ça ?",
-                    "Gracias ! https://owncloud.vgaudard.com/index.php/apps/files_sharing/ajax/publicpreview.php?x=1366&y=322&a=true&file=gracias_dinosauro.png&t=FtNRWKa9T9nkb7o&scalingup=0",
-                    "Ah! Je savais que ça allait te plaire.",
-                    "Tu trouves ? https://cdn.drawception.com/images/panels/2015/3-28/Gdj3GYHa8C-5.png",
-                    "Yo solo hace mi trabajo ! No puede usted pagarme ? Tengo hambre...",
-                    "谢谢大家！谢谢你，我不会让我鞭那一夜！",
-                    "Cool ! J'avais peur de *perdre* mon travail.",
-                    "kthxbye",
-                    "C'est un plaisir de faire ça pour toi !",
-                    "J'ai un bon professeur !",
-                    "Bon, on va pas en faire tout un plat non plus.",
-                    "Wesh gro sa fé zizir https://s-media-cache-ak0.pinimg.com/736x/0e/22/ed/0e22ed251bdf256052685cfbbfe30a69.jpg",
-                    "Danke https://i.ytimg.com/vi/hPutWCpbFBs/maxresdefault.jpg",
-                    "Rawr matey, i be glad you like this, but i be no squiffy ! Now where's them doubloons ? https://ih1.redbubble.net/image.120312598.0390/flat,800x800,075,f.u2.jpg",
-                    "Dis plutôt merci à mon maitre adoré ! C'est grâce à lui que je sais faire tout ça.",
-                    "https://www.youtube.com/watch?v=j3IJQXwah0E",
-                    "https://s-media-cache-ak0.pinimg.com/originals/a6/c1/89/a6c189a2dee8a689eb4bb1688c03dd0c.jpg",
-                    "https://www.youtube.com/watch?v=OcfqDPAy7zc&t=3",
-                    "https://www.youtube.com/watch?v=79DijItQXMM",
-                    "https://owncloud.vgaudard.com/index.php/apps/files_sharing/ajax/publicpreview.php?x=1366&y=322&a=true&file=elvish_welcome.png&t=N0NFTXOQNOV4UZR&scalingup=0",
-                    "Tu crois que je fais ça pour toi ?",
-                    "Ouais, je trouve aussi que c'était cool !"]
+diplodocus = Bot.new
+thanks_answers = JSON.parse(File.read("thanks.json"))
 
-# This should not be a problem unless the server stays on at all times, which it does now, but idc
-messagesAnswered = {}
-
-tagueuletime = 0
-
-#bot.message(in: "#general") do |event|
-bot.message() do |event|
-    if event.timestamp.to_i < tagueuletime + 15 * 60
-        return
+diplodocus.addMessageReaction(merciregex, lambda { |event|
+    if not notregex.match(event.content)
+        return thanks_answers.sample
     end
-    if tagueuleregex.match(event.content)
-        tagueuletime = event.timestamp.to_i
-    elsif merciregex.match(event.content) && event.timestamp.to_i - lastmessagetime.to_i > 5 && !event.from_bot?
-        if not notregex.match(event.content)
-            mess = thanks_answers.sample
-            event.respond mess
-            lastmessagetime = event.timestamp
-        end
-    elsif awesomeregex.match(event.content) && event.timestamp.to_i - lastmessagetime.to_i > 5 && !event.from_bot?
-        method = event.content[awesomeregex, 1].downcase
-        mess = event.content[awesomeregex, 2]
-        send = true
-        if awesomeregexofnodoublon.match(mess)
-            mess = mess[awesomeregexofnodoublon, 2]
-        end
-        if method == "cri"
-            mess.upcase!
-        elsif method == "di" or method == "dy"
-            mess.capitalize!
-        end
-        if /b[ilíìî|!*()\\\/][sz][uù]/i.match(mess)
-            message = message = event.respond "JAMAIS JE NE MANQUERAI DE RESPECT À MON SEIGNEUR ET MAITRE!"
-        elsif /^Rect$/.match(mess)
-            message = event.respond "GET REKT!"
-        elsif /^Sputes?$/.match(mess)
-            message = event.respond "PUTE!"
-        elsif /^jkstra$/i.match(mess)
-            message = event.respond "jsk... jkst... jkj... Roy-Warshall!"
-        else
-            if rand(5) == 0
-                message = event.respond mess
-                send = true
-            end
-        end
-        if send
-            lastmessagetime = event.timestamp
-            messagesAnswered[ event.message.id ] = message
-        end
+}, 80)
+
+diplodocus.addMessageReaction(tagueuleregex, lambda { |event|
+    diplodocus.setTagueuleTime( event.timestamp.to_i )
+})
+
+diplodocus.addMessageReaction(awesomeregex, lambda { |event|
+    method = event.content[awesomeregex, 1].downcase
+    mess = event.content[awesomeregex, 2]
+    if awesomeregexofnodoublon.match(mess) # Remove leading duplicate characters
+        mess = mess[awesomeregexofnodoublon, 2]
     end
-end
-
-bot.message ({contains: /\balarm(?:er?|(é|ant)e?s?)?\b/i}) do |event|
-    if event.timestamp.to_i - lastmessagetime.to_i > 5 && !event.from_bot?
-        lastmessagetime = event.timestamp
-        event.respond("ALARME!")
-        event.respond("https://www.youtube.com/watch?v=TqDsMEOYA9g")
+    case method
+    when /^d[iy]/i
+        mess.capitalize!
+    when /^cr[iy]/i
+        mess.upcase!
+    else
+        mess.downcase!
     end
-end
+    return mess
+}, 50, 20, lambda { |mess|
+    return mess.author.display_name + " est lâche!"
+})
 
+diplodocus.addMessageReaction(/\bdirect\b/i, lambda { |event|
+    return "GET REKT!"
+}, 200)
 
-bot.message_edit() do |event|
-    mess = event.message
-    if messagesAnswered.key?(mess.id)
-        messagesAnswered[ mess.id ].edit(mess.author.display_name + " est lâche!")
-    end
-end
+diplodocus.addMessageReaction(/\bdispute\b/i, lambda { |event|
+    return "PUTE!"
+}, 200)
 
-bot.run
+diplodocus.addMessageReaction(/\bdijkstra\b/i, lambda { |event|
+    return "jsk... jkst... jkj... Roy-Warshall!"
+}, 200)
 
+diplodocus.addMessageReaction(/\balarm(?:er?|(é|ant)e?s?)?\b/i, lambda { |event|
+        return "ALARME!\nhttps://www.youtube.com/watch?v=TqDsMEOYA9g"
+}, 200)
+
+diplodocus.addMessageReaction(/b[ilíìî][sz][uù]/i, lambda { |event|
+    return "JAMAIS JE NE MANQUERAI DE RESPECT À MON SEIGNEUR ET MAITRE!"
+}, 200)
+
+diplodocus.run
---
+++ root.rb
@@ -0,0 +1,87 @@
+#!/usr/bin/ruby
+
+require 'discordrb'
+
+MessageEditReaction = Struct.new(:message, :reaction, :date)
+MessageReaction = Struct.new(:regex, :answerFunction, :priority, :probability, :editReactionFunction)
+
+class Bot
+
+    def initialize()
+        token = File.read("token").strip
+        applicationId = File.read("application_id").to_i
+        @bot = Discordrb::Bot.new token: token, client_id: applicationId
+        @tagueule_lasttime = -1
+        @tagueule_defaultdelay = 15 * 60 # 15 minutes
+        @lastmessagetime = -1
+        @message_delay = 5 # 5 seconds
+        @cleaning_delay = 60 * 60 * 24 # 1 day
+        @cleaning_lasttime = Time.now
+        @reactions = []
+        @messagesAnswered = {}
+
+        ### Message edit management
+        @bot.message_edit() do |event|
+            mess = event.message
+            if @messagesAnswered.key?(mess.id)
+                messageEditReaction = @messagesAnswered[mess.id]
+                messageEditReaction.message.edit(messageEditReaction.reaction.call(mess))
+            end
+        end
+
+        ### Message answer management
+        @bot.message() do |event|
+            reaction = nil
+            priorityMax = -1
+            @reactions.each { |reac|
+                if reac.priority > priorityMax && reac.regex.match(event.content)
+                    priorityMax = reac.priority
+                    reaction = reac
+                end
+            }
+            date = event.timestamp.to_i
+            if (date < @tagueule_lasttime + @tagueule_defaultdelay) || (reaction == nil) || (date - @lastmessagetime < @message_delay) || (event.from_bot?) || (rand(100) == reaction.probability)
+                next
+            end
+            answer = reaction.answerFunction.call(event)
+            @lastmessagetime = date
+            message = event.respond(answer)
+            if reaction.editReactionFunction != nil
+                messageEditReaction = MessageEditReaction.new(message, reaction.editReactionFunction, Time.now)
+                @messagesAnswered[event.message.id] = messageEditReaction
+            end
+        end
+
+        ### Cleaning management
+        Thread.new do
+            loop do
+                sleep cleaning_delay
+                @messagesAnswered.delete_if { |k,v| v.date < @cleaning_lasttime }
+                @cleaning_lasttime = Time.now
+            end
+        end
+    end
+
+    # Adds a possible reaction
+    # Note: There is only one reaction to a single message
+    # Params:
+    # +messageRegex+:: Regular expression to check if this reaction should be used against a given message
+    # +answerFunction+:: Function returning a string containing the message to be sent. It has a single parameter: the event object
+    # +priority+:: Priority used to select the reaction. The selected reaction is the one with the highest priority.
+    # +probability+:: Percentage of the time this reaction should be used. (Used after selecting a reaction)
+    # +editReactionFunction+:: Function returning a string containing the new content of the message to be edited. It is called when an answered message is edited.
+    def addMessageReaction(messageRegex, answerFunction, priority = 100, probability = 100, editReactionFunction = nil)
+        @reactions.push(MessageReaction.new(messageRegex, answerFunction, priority, probability, editReactionFunction))
+    end
+
+    def setTagueuleTime(time)
+        @tagueule_lasttime = time
+    end
+
+    def run
+        @bot.run
+    end
+end
+
+
+# Because over-engineered jokes are always the best
