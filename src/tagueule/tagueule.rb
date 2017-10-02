129e7769a45c2d42ee1137731ec13e75e5e1f85f
Victor Gaudard <victor.gaudard@gmail.com>
2017-10-02 18:44:41 +0200

    [Tagueule] Replace if's with a case + remove help

--- src/tagueule/tagueule.rb
+++ src/tagueule/tagueule.rb
@@ -6,11 +6,6 @@
 # Use `!dip pause [durations]` to pause reactions (durations in seconds, defaults to 15*60)
 class TaGueule
     def initialize(shortDelay = 15 * 60)
-        @stopRegex = /^!dip stop$|meurs.*dip/i
-        @startRegex = /^!dip start$|reviens.*dip/i
-        @shortPauseRegex = /^!dip pause$|ta gueule.*dip|tais.toi.*dip/i
-        @pauseRegex = /^!dip pause (\S+)$/i
-        @helpRegex = /^!dip help$/
         @shortDelay = shortDelay
         @until = Time.now
         @stopped = false
@@ -21,21 +16,16 @@ class TaGueule
     end
 
     def reactTo(event)
-        if @stopRegex.match event.content
+        case event.content
+        when /^dip stop$|meurs.*dip/i
             @stopped = true
-        elsif @startRegex.match event.content
+        when /^!dip start$|reviens.*dip/i
             @until = Time.now
             @stopped = false
-        elsif @shortPauseRegex.match event.content
+        when /^!dip pause$|ta gueule.*dip|tais.toi.*dip/i
             @until = Time.now + @shortDelay
-        elsif @pauseRegex.match event.content
-            delay = event.content[@pauseRegex].to_i
-            @until = Time.now + delay
-        elsif @helpRegex.match event.content
-            return \
-                "`!dip stop`: prevent reactions until `!dip start`\n" + \
-                "`!dip start`: enable reactions\n" + \
-                "`!dip pause [durations]`: pause reactions (durations in seconds, defaults to 15*60)\n"
+        when /^!dip pause (\S+)$/i
+            @until = Time.now + $1.to_i
         end
     end
 end
