e7c7bc6e20077480d6d965c2a8c72903513ca1d7
Victor Gaudard <victor.gaudard@gmail.com>
2017-10-06 19:43:10 +0200

    [Writing Clock] Keep and lose dignity
    Is anyone actually reading this ?

--- src/writingclock/writingclock.rb
+++ src/writingclock/writingclock.rb
@@ -6,7 +6,7 @@ class WritingClock
         if event.content == "!time"
             randomTime = rand(28).to_s.rjust(2, '0') + "h" + (rand (90) - 15).to_s.rjust(2, '0')
             return \
-                "<:norm:356744948013596673>: `" + getTime('+02:00') + "`\n" + \
+                "<:bzh:356744042224025600>: `"  + getTime('+02:00') + "`\n" + \
                 ":cat:: `"                      + getTime('+09:00') + "`\n" + \
                 ":leaves:: `"                   + getTime('+07:00') + "`\n" + \
                 "<:kilt:356831960183603210>: `" + getTime('+01:00') + "`\n" + \
