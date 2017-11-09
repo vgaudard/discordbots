9b8074c0c5d3b4dcb8e3f13e059fe2d0b047e57e
Victor Gaudard <victor.gaudard@gmail.com>
2017-11-09 09:27:26 +0100

    [Writing clock] DST

--- src/writingclock/writingclock.rb
+++ src/writingclock/writingclock.rb
@@ -6,11 +6,11 @@ class WritingClock
         if event.content == "!time"
             randomTime = rand(28).to_s.rjust(2, '0') + "h" + (rand (90) - 15).to_s.rjust(2, '0')
             return \
-                "<:bzh:356744042224025600>: `"  + getTime('+02:00') + "`\n" + \
+                "<:bzh:356744042224025600>: `"  + getTime('+01:00') + "`\n" + \
                 ":cat:: `"                      + getTime('+09:00') + "`\n" + \
                 ":leaves:: `"                   + getTime('+07:00') + "`\n" + \
-                "<:kilt:356831960183603210>: `" + getTime('+01:00') + "`\n" + \
-                "<:nom:356747206822461440>: `"  + getTime('+02:00') + "`\n" + \
+                "<:kilt:356831960183603210>: `" + getTime('+00:00') + "`\n" + \
+                "<:nom:356747206822461440>: `"  + getTime('+01:00') + "`\n" + \
                 ":pizza:: `"                    + randomTime        + "`\n"
         end
     end
