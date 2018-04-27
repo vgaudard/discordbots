256d6f24005f0a1b6ec1db05e1ba2b9982cede5f
Victor Gaudard <victor.gaudard@gmail.com>
2018-04-27 12:35:16 +0000

    [Josie] Fix message

--- src/plugins/josie.rb
+++ src/plugins/josie.rb
@@ -5,7 +5,7 @@ class Josie
     def reactTo(event)
         case event.content
         when /josie|josiane/i
-            return ["**RAPPEL**", "TRES IMPORTANT", "**RAPPEL TRES IMPORTANT**", "Je ne vous le montre pas souvent mais je vous aime, mes petits choupinoux." "**RAPPEL** JE PARS EN PAUSE CAFE. LE BUREAU SERA FERME DE 16H06 A  16H12."].sample
+            return ["**RAPPEL**", "TRES IMPORTANT", "**RAPPEL TRES IMPORTANT**", "Je ne vous le montre pas souvent mais je vous aime, mes petits choupinoux.", "**RAPPEL 3**\nJE PARS EN PAUSE CAFE. LE BUREAU SERA FERME DE 16H06 A  16H12."].sample
         when /rappel/i
             return "**MES CHOUPINOUX D'AMOUR**"
         end
