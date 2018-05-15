e5e1fca2f2456343a22098c79cebb209ec0e901b
Victor Gaudard <victor.gaudard@gmail.com>
2018-05-15 11:41:30 +0000

    [Kaamelott] Add alias

--- src/plugins/kaamelott.rb
+++ src/plugins/kaamelott.rb
@@ -3,7 +3,7 @@
 ## Kaamelott
 class Kaamelott
     def reactTo(event)
-        linkregex = /https:\/\/kaamelott-soundboard.2ec0b4.fr\/#son\/([a-zA-Z0-9_-]+)/i
+        linkregex = /(?:https:\/\/kaamelott-soundboard.2ec0b4.fr\/#son\/|^!ksb\s+)([a-zA-Z0-9_-]+)/i
         if linkregex.match event.content
             id = event.content[linkregex, 1]
             mp3path = File.join(File.dirname(__FILE__), "..", "sounds", id + ".mp3")
