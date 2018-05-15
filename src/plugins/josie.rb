ecb703c14cdfd26c4d58e4a02779f576c8c79c06
Victor Gaudard <victor.gaudard@gmail.com>
2018-05-15 09:17:41 +0000

    [Josie] Fix word limit and add a sentence

--- src/plugins/josie.rb
+++ src/plugins/josie.rb
@@ -5,8 +5,8 @@ class Josie
     def reactTo(event)
         case event.content
         when /josie|josiane/i
-            return ["**RAPPEL**", "TRES IMPORTANT", "**RAPPEL TRES IMPORTANT**", "Je ne vous le montre pas souvent mais je vous aime, mes petits choupinoux.", "**RAPPEL 3**\nJE PARS EN PAUSE CAFE. LE BUREAU SERA FERME DE 16H06 A  16H12."].sample
-        when /rappel/i
+            return ["**RAPPEL**", "TRES IMPORTANT", "**RAPPEL TRES IMPORTANT**", "Je ne vous le montre pas souvent mais je vous aime, mes petits choupinoux.", "**RAPPEL 3**\nJE PARS EN PAUSE CAFE. LE BUREAU SERA FERME DE 16H06 A  16H12.", "Je ne vous aime pas. Merci de prendre vos dispositions, notamment pour ceux qui auraient l'horrible envie de venir me voir"].sample
+        when /\brappel\b/i
             return "**MES CHOUPINOUX D'AMOUR**"
         end
     end
